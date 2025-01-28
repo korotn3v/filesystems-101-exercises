3dc2eea36e0de0c428e8b90208e306e525b76fdf
Anton <korotnev09@gmail.com>
2025-01-28 22:21:35 +0200

    random 03

--- 02-fuse-helloworld/solution.h
+++ 02-fuse-helloworld/solution.h
@@ -4,23 +4,18 @@
 
 /**
    Implement this function to to mount a "helloworld" FUSE filesystem to @mntp.
-   The root of a filesystem must contain the following entries:
+   The root of a filesystem must contain the following entries (the owner UID
+   and GID may be different):
 
    $ ls -lha /helloworld
-   drwxrwxr-x. 2 artem artem 19 Oct  6 05:39 .
-   drwxr-xr-x. 4 artem artem 99 Oct  6 05:39 ..
-   -r--------. 1 artem artem  0 Oct  6 05:39 hello
+   drwxrwxr-x. 2 9091 9091 19 Oct  6 05:39 .
+   drwxr-xr-x. 4 9091 9091 19 Oct  6 05:39 ..
+   -r--------. 1 9091 9091 19 Oct  6 05:39 hello
 
-   The owner UID and GID of "hello" must be reported as the effective UID
-   and GID of a process that lists the directory. For example,
+   The content of "hello" must be (without quotes) "hello, ${PID}\n" where
+   PID is the id of a process that reads "hello", and "\n" is the newline.
+   It is OK to report the size of "hello" that does not match the content.
 
-   $ sudo ls -lha /helloworld
-   drwxrwxr-x. 2 root root 19 Oct  6 05:39 .
-   drwxr-xr-x. 4 root root 99 Oct  6 05:39 ..
-   -r--------. 1 root root  0 Oct  6 05:39 hello
-
-   The content of "hello" must be (without quotes) "hello, ${PID}" where
-   PID is the id of a process that reads "hello". It is OK to report the
-   size of "hello" that does not match the content of it.
+   Any attempt write to the FS must report EROFS.
 */
 int helloworld(const char *mntp);
--- 03-io_uring/solution.c
+++ 03-io_uring/solution.c
@@ -1,44 +1,147 @@
-#include <solution.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fs_malloc.h>
+#include "solution.h"
+
+#include <liburing.h>
+#include <stdlib.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int copy(int in, int out)
-{
-	struct stat st_in, st_out;
-	int r;
 
-	if ((r = fstat(in, &st_in)) ||
-	    (r = fstat(out, &st_out)))
-	{
-		return -errno;
-	}
+#define QUEUE_CAPACITY 4
+#define BLOCKSIZE 256 * 1024
 
-	size_t len = st_in.st_size;
-	void *buf = fs_xmalloc(len);
-	r = pread(in, buf, len, 0);
-	if (r < 0) {
-		r = -errno;
-		goto out;
-	}
-	if ((size_t)r != len) {
-		r = -EAGAIN;
-		goto out;
-	}
+struct req_data {
+    int is_read;
+    off_t offset;
+    size_t buf_size;
+    char* buffer;
+};
 
-	r = write(out, buf, len);
-	if (r < 0) {
-		r = -errno;
-		goto out;
-	}
-	if ((size_t)r != len) {
-		r = -EAGAIN;
-		goto out;
-	}
+int read_q(struct io_uring* ring, size_t buf_size, off_t offset, int fd) {
 
-	r = 0;
-out:
-	fs_xfree(buf);
-	return r;
+    struct io_uring_sqe* sqe = io_uring_get_sqe(ring);
+    if (!sqe) {
+        return 1;
+    }
+
+    struct req_data* req_data = (struct req_data*) malloc(buf_size + sizeof(*req_data));
+    if (!req_data) {
+        return 1;
+    }
+
+    req_data->is_read  = 1;
+    req_data->offset  = offset;
+    req_data->buf_size = buf_size;
+    req_data->buffer = (char*)req_data + sizeof(*req_data);
+
+    io_uring_prep_read(sqe, fd, req_data->buffer, buf_size, offset);
+    io_uring_sqe_set_data(sqe, req_data);
+
+    return 0;
+}
+
+int write_q(struct io_uring* ring, struct req_data* req_data, int fd) {
+
+    struct io_uring_sqe* sqe = io_uring_get_sqe(ring);
+    if (!sqe) {
+        return 1;
+    }
+
+    req_data->is_read = 0;
+
+    io_uring_prep_write(sqe, fd, req_data->buffer, req_data->buf_size, req_data->offset);
+    io_uring_sqe_set_data(sqe, req_data);
+
+    return 0;
+}
+
+int copy(int in, int out) {
+
+    struct io_uring ring;
+    if (io_uring_queue_init(QUEUE_CAPACITY, &ring, 0) < 0) {
+        return -errno;
+    }
+
+    struct stat st;
+    if (fstat(in, &st) < 0) {
+        return -errno;
+    }
+    off_t read_left = st.st_size;
+    off_t write_left = read_left;
+    off_t offset = 0;
+
+    int reads_q = 0;
+    int writes_q = 0;
+
+    struct io_uring_cqe* cqe;
+
+    while (read_left || write_left || writes_q) {
+
+        int read_flag = 0;
+        while (read_left) {
+
+            if (reads_q + writes_q == QUEUE_CAPACITY) {
+                break;
+            }
+
+            size_t buf_size;
+            if (read_left > BLOCKSIZE) {
+                buf_size = BLOCKSIZE;
+            } else {
+                buf_size = read_left;
+            }
+
+            if (read_q(&ring, buf_size, offset, in)) {
+                break;
+            }
+
+            read_left -= buf_size;
+            offset += buf_size;
+            reads_q++;
+            read_flag = 1;
+        }
+
+        if (read_flag) {
+            if (io_uring_submit(&ring) < 0) {
+                return -errno;
+            }
+        }
+
+        int get_cqe_flag = 0;
+
+        while (write_left || writes_q) {
+
+            if (!get_cqe_flag) {
+                if (io_uring_wait_cqe(&ring, &cqe) < 0) {
+                    return -errno;
+                }
+                get_cqe_flag = 1;
+            } else {
+                if (io_uring_peek_cqe(&ring, &cqe) == -EAGAIN) {
+                    break;
+                }
+            }
+
+            struct req_data* req_data = io_uring_cqe_get_data(cqe);
+
+            if (req_data->is_read) {
+
+                if (write_q(&ring, req_data, out)) {
+                    break;
+                }
+
+                io_uring_submit(&ring);
+                reads_q--;
+                writes_q++;
+                write_left -= req_data->buf_size;
+            } else {
+                free(req_data);
+                writes_q--;
+            }
+            io_uring_cqe_seen(&ring, cqe);
+        }
+    }
+    io_uring_queue_exit(&ring);
+    return 0;
 }
