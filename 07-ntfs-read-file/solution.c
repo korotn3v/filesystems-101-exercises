7184a88c00ef729c3ca533bb4d3129d06660261b
Anton <korotnev09@gmail.com>
2025-01-28 22:32:10 +0200

    07

--- 07-ntfs-read-file/solution.c
+++ 07-ntfs-read-file/solution.c
@@ -1,69 +1,64 @@
-#include <solution.h>
+#include "solution.h"
+#include <ntfs-3g/ntfs.h>
 #include <errno.h>
 #include <unistd.h>
-#include "fs_malloc.h"
-#include "fs_string.h"
-#include <ntfs-3g/device.h> 
-#include <ntfs-3g/inode.h>
-#include <ntfs-3g/volume.h>
-#include <ntfs-3g/attrib.h>
-#include <ntfs-3g/runlist.h>
+#include <fcntl.h>
+#include <string.h>
 
+#define BUFFER_SIZE 4096
 
-int dump_file(int img, const char *path, int out) {
-    ntfs_volume *vol;
-    ntfs_inode *inode;
-    ntfs_attr *attr;
-    char *buf;
-    s64 offset = 0;
-    const int buf_size = 4096;  // Размер буфера для чтения
-    int bytes_read;
-    int result = 0;
+int dump_file(int img, const char *path, int out)
+{
+	ntfs_volume *vol;
+	ntfs_file *file;
+	ntfs_inode *inode;
+	s64 bytes_read;
+	char buffer[BUFFER_SIZE];
+	ssize_t write_result;
+	int ret = 0;
 
-    vol = ntfs_device_mount(img, 0);
-    if (!vol) {
-        return -errno;
-    }
+	/* Mount the NTFS volume from the image file descriptor */
+	vol = ntfs_mount_fd(img, 0);
+	if (!vol) {
+		return -errno;
+	}
 
-    inode = ntfs_pathname_to_inode(vol, NULL, path);
-    if (!inode) {
-        ntfs_umount(vol, FALSE);
-        return -errno;
-    }
+	/* Open the file using libntfs-3g */
+	file = ntfs_file_open(vol, path, O_RDONLY);
+	if (!file) {
+		ret = -errno;
+		goto cleanup_volume;
+	}
 
-    if (!NF_IS_REG(inode)) {
-        ntfs_inode_close(inode);
-        ntfs_umount(vol, FALSE);
-        return -EINVAL;
-    }
+	/* Get the inode for additional file information if needed */
+	inode = ntfs_pathname_to_inode(vol, NULL, path);
+	if (!inode) {
+		ret = -errno;
+		goto cleanup_file;
+	}
 
-    attr = ntfs_attr_open(inode, AT_DATA, NULL, 0);
-    if (!attr) {
-        ntfs_inode_close(inode);
-        ntfs_umount(vol, FALSE);
-        return -errno;
-    }
+	/* Read from NTFS file and write to output descriptor */
+	while ((bytes_read = ntfs_file_read(file, buffer, sizeof(buffer))) > 0) {
+		write_result = write(out, buffer, bytes_read);
+		if (write_result < 0) {
+			ret = -errno;
+			goto cleanup_inode;
+		}
+		if (write_result != bytes_read) {
+			ret = -EIO;
+			goto cleanup_inode;
+		}
+	}
 
-    buf = fs_xmalloc(buf_size);
+	if (bytes_read < 0) {
+		ret = -errno;
+	}
 
-    while ((bytes_read = ntfs_attr_pread(attr, offset, buf_size, buf)) > 0) {
-        ssize_t bytes_written = write(out, buf, bytes_read);
-        if (bytes_written != bytes_read) {
-            result = -errno;
-            break;
-        }
-        offset += bytes_read;
-    }
-
-    if (bytes_read < 0) {
-        result = -errno;
-    }
-
-    fs_xfree(buf);
-    ntfs_attr_close(attr);
-    ntfs_inode_close(inode);
-    ntfs_umount(vol, FALSE);
-
-    return result;
+	cleanup_inode:
+		ntfs_inode_close(inode);
+	cleanup_file:
+		ntfs_file_close(file);
+	cleanup_volume:
+		ntfs_umount(vol, FALSE);
+	return ret;
 }
-
